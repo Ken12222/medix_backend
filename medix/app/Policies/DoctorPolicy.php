2ba2f076884ca6e54bca62a408d158fba40cf390
Ken <ashartey2@gmail.com>
2024-09-22 22:46:40 +0000

    doctor route protection

---
+++ medix/app/Policies/DoctorPolicy.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Doctor;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class DoctorPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(?User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(?User $user, Doctor $doctor): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Doctor $doctor): bool
+    {
+        if($user->id === $doctor->user_id){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Doctor $doctor): bool
+    {
+        if($user->id === $doctor->user_id){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
