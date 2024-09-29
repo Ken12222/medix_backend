268ad5739e3d9ab3e3b54d76b26eea3ad2ee61ea
Ken <ashartey2@gmail.com>
2024-09-29 21:58:41 +0000

    Email verification

---
+++ medix/app/Notifications/EmailVerificationNotification.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class EmailVerificationNotification extends Notification
+{
+    use Queueable;
+
+
+    protected $url;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject("Email Verification")
+                    ->line('Dear'. $notifiable, )
+                    ->line('Please click Verify to verify your email. Ignore this message if you did not register to our platform')
+                    ->action('Verify Email', url($this->url))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
